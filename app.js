2b2b9f8bfe56853981442d54a05854e6ca797658
kiara296 <kiaraheinrich@hotmail.com>
2021-10-11 22:20:16 -0300

    agregue modificaciones para que funcione en heroku

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const path = require('path');
 
 app.use(express.static('public'));
 
-/* app.use(express.urlencoded()); */
-
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'))
@@ -22,14 +20,11 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/login.html'))
 });
 
-/* app.post("/register", (req, res) => {
-    console.log(req.body)
-}); */
 
 app.post("/resultadoRegister", (req, res) => {
     res.redirect("/")
 });
 
-app.listen(3030, () =>
+app.listen(process.env.PORT || 3030, () =>
     console.log('Servidor corriendo')
 )
